c65c84e84f1fc9a49f7ed6d8059f043c99dee6af
sanarajan <sanamol87@gmail.com>
2026-06-07 23:46:05 +0530

    update payment and cloudinary url

--- src/index.ts
+++ src/index.ts
@@ -126,7 +126,7 @@ app.use('/api/user/payment-settings', userPaymentSettingsRoutes);
 // Error Handler
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5003;
 
 app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
